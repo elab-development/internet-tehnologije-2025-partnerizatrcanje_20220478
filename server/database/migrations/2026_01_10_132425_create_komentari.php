e2880a41e8ad5f237df9c08ca8b01f11feb1c920
Pavle Milovanovic <mpavle123@gmail.com>
2026-01-10 14:42:11 +0100

    Migracije

---
+++ server/database/migrations/2026_01_10_132425_create_komentari.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('komentari', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('komentar');
+            $table->dateTime('datum_komentara');
+            $table->integer('ocena');
+
+            $table->foreign('post_id')->references('id')->on('postovi')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('komentari');
+    }
+};
